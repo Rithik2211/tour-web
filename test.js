a0b91ce92e748e5fd4c95c93206ab3443f3aa2e5
Rithik2211 <rithikpranao22@gmail.com>
2025-04-20 21:40:37 +0530

    test data

---
+++ test.js
@@ -0,0 +1,8 @@
+arr = [1,4,56,7,3,4,5,6]
+
+console.log("AT : ", arr.at(2))
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+const calcFucn = (value) => value < 30;
+console.log("every : ", array1.every(calcFucn));
