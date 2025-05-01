e00510ddd73b7829eb8c165aa2c42281234c7196
Rithik2211 <rithikpranao22@gmail.com>
2025-05-01 20:16:27 +0530

    added text

--- app/screens/AboutUs/page.tsx
+++ app/screens/AboutUs/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <div className='h-screen w-full px-[20px] pt-[50px] flex flex-col'>
-        {/* <h1 className='text-black font-bold'>About Page</h1> */}
+        <h1 className='text-black font-bold'>About Page</h1>
     </div>
   )
 }
